e1295d391b939ade7db99cd9432d46c2aaa35d9b
Robert Galat <robgal519@gmail.com>
2020-06-16 22:56:08 +0200

    add actuator for admin

--- sc-application/src/main/java/pl/agh/shopping/card/application/config/SecurityConfig.java
+++ sc-application/src/main/java/pl/agh/shopping/card/application/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig, tokenHolder), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
+                .antMatchers("/actuator/**").hasRole("ROLE_ADMIN")
                 .anyRequest().authenticated();
     }
 
